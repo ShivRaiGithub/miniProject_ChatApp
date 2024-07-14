c0c37f52a8b3a5e3e83ff0e8036acc67f89bda48
Shiv Rai <sshaktirai@gmail.com>
2024-07-14 11:12:27 +0530

    3

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex h-screen w-screen rounded-lg overflow-hidden p-4 m-10 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-		{/* <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'> */}
 			<Sidebar />
 			<MessageContainer />
 		</div>
