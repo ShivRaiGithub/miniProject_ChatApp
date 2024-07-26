f135e21ff840b35c7187ce118933a97804d41599
Shiv Rai <111505548+ShivRaiGithub@users.noreply.github.com>
2024-07-26 19:28:31 +0530

    Update

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://sshaktirai:909090@cluster0.7zyxom8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect("");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
